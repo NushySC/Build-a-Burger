b11b28844f858aeacec1158a7a0662eea106aee2
NushySC <ank.sampedro@gmail.com>
2019-04-09 14:22:43 +0200

    done for now

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 import styles from './Modal.module.css'
-import Auxi from '../../hoc/Auxi'
+import Auxi from '../../hoc/Auxi/Auxi'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-<Auxi >
-    <Backdrop show={props.show} clicked={props.modalClosed} />
+class Modal extends Component {
+
+    shouldComponentUpdate( nextProps, nextState ) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate () {
+
+    }
+
+    render() {
+        return (
+            <Auxi >
+    <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
         <div className={styles.Modal}
         style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
         }}>
-            {props.children}
+            {this.props.children}
         </div>
 </Auxi>
-);
+        )
+    }
+    }
 
-export default modal;
+export default Modal;

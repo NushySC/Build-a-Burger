0c0b231260613536d50574a8d3caeca2859cce24
Anuska <ank.sampedro@gmail.com>
2019-07-12 15:54:13 +0200

    validation

--- src/UI/Button/Button.js
+++ src/UI/Button/Button.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import styles from'./Button.module.css';
+import React from "react";
+import styles from "./Button.module.css";
 
-const button = (props) => (
-    <button
-    className={[styles.button, styles[props.btnType]].join(' ')}
-    onClick={props.clicked}>{props.children}</button>
+const button = props => (
+  <button
+    disabled={props.disabled}
+    className={[styles.button, styles[props.btnType]].join(" ")}
+    onClick={props.clicked}
+  >
+    {props.children}
+  </button>
 );
 
 export default button;

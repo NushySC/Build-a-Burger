6739920fa563d6bc56957d2a61f0fe25d8058b3b
Anuska <ank.sampedro@gmail.com>
2019-07-13 14:30:02 +0200

    action creators

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 import './index.css';
 import App from './App';
-import reducer from './store/reducer'
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 //import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer)
-
-const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+const store = createStore(
+	burgerBuilderReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render( app, document.getElementById( 'root' ) );
+const app = (
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 //registerServiceWorker();
